6b7f0c99ca65659ede396483631c4dfacea172c1
statuc30721 <179263776+statuc30721@users.noreply.github.com>
2026-05-15 19:26:30 +0200

    Update 2-main.tf

--- Individual-Homework/09-week-09/terraform/2-main.tf
+++ Individual-Homework/09-week-09/terraform/2-main.tf
@@ -66,9 +66,52 @@ resource "google_compute_instance_group_manager" "mephisto_mig" {
   base_instance_name = "mephisto"
   zone               = var.zone
 
+  target_size = var.mig_target_size
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+
   version {
     instance_template = google_compute_instance_template.mephisto_template.self_link
   }
 
-  target_size = var.mig_target_size
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.mephisto_health_check.self_link
+    initial_delay_sec = 300
+
+  }
+}
+
+# Autoscaler configuration for the MIG.
+resource "google_compute_autoscaler" "mephisto_autoscaler" {
+  name   = "mephisto-autoscaler"
+  zone   = var.zone
+  target = google_compute_instance_group_manager.mephisto_mig.id
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.6
+    }
+  }
+}
+
+# Health check configuration.
+resource "google_compute_health_check" "mephisto_health_check" {
+  name                = "mephisto-health-check"
+  check_interval_sec  = 30
+  timeout_sec         = 10
+  healthy_threshold   = 3
+  unhealthy_threshold = 3
+
+  http_health_check {
+    port         = 80
+    request_path = "/"
+  }
 }
