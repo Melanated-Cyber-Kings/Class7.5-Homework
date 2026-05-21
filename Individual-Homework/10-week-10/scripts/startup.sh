df229939799110f69002abc6bb496d8a85ad1a79
statuc30721 <179263776+statuc30721@users.noreply.github.com>
2026-05-21 19:28:27 +0200

    Update startup.sh
    cleaned up the script. Had issues dye to type on line 20

--- Individual-Homework/10-week-10/scripts/startup.sh
+++ Individual-Homework/10-week-10/scripts/startup.sh
@@ -17,24 +17,11 @@ METADATA_FLAVOR_HEADER="Metadata-Flavor: Google"
 local_ipv4=$(curl -H "${METADATA_FLAVOR_HEADER}" -s "${METADATA_URL}/instance/network-interfaces/0/ip")
 zone=$(curl -H "${METADATA_FLAVOR_HEADER}" -s "${METADATA_URL}/instance/zone")
 project_id=$(curl -H "${METADATA_FLAVOR_HEADER}" -s "${METADATA_URL}/project/project-id")
-@ I commented out network tags since it is not being used.
+# I commented out network tags since it is not being used.
 #network_tags=$(curl -H "${METADATA_FLAVOR_HEADER}" -s "${METADATA_URL}/instance/tags")
 student_name=$(curl -H "${METADATA_FLAVOR_HEADER}" -s "${METADATA_URL}/instance/attributes/student_name")
 
-# Previous version commented out.
-# Create a simple HTML page with instance metadata
-# cat <<EOF > /var/www/html/index.html
-# <html><body>
-# <h2>REGION A</h2>
-# <h3>Created with a direct input startup script!</h3>
-# <p><b>Student Name:</b> $student_name</p>
-# <p><b>Instance Name:</b> $(hostname -f)</p>
-# <p><b>Instance Private IP Address:</b> $local_ipv4</p>
-# <p><b>Zone:</b> $zone</p>
-# <p><b>Project ID:</b> $project_id</p>
-# <p><b>Network Tags:</b> $network_tags</p>
-# </body></html>
-# EOF
+
 
 # Trying this one to get better formatting and output. My HTML skills need work.
 cat <<EOF > /var/www/html/index.html
