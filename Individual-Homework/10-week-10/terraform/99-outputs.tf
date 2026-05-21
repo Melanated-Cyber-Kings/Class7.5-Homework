1300aec7e9c5c440b901b19a3e459bf43073cb3d
statuc30721 <179263776+statuc30721@users.noreply.github.com>
2026-05-21 22:41:44 +0200

    Update 99-outputs.tf
    updated output

--- Individual-Homework/10-week-10/terraform/99-outputs.tf
+++ Individual-Homework/10-week-10/terraform/99-outputs.tf
@@ -1,11 +1,11 @@
 output "mig_name" {
   description = "Name of the Managed Instance Group."
-  value       = google_compute_instance_group_manager.mephisto_mig.name
+  value       = google_compute_region_instance_group_manager.mephisto_mig.name
 }
 
 output "mig_size" {
   description = "Number of instances in the MIG."
-  value       = google_compute_instance_group_manager.mephisto_mig.target_size
+  value       = google_compute_region_instance_group_manager.mephisto_mig.target_size
 }
 
 output "load_balancer_ip" {
